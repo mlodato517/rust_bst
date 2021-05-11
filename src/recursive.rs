f2f0e46e47ee9a89558fab14a4c059afc416f644
Mark Lodato <mark@gnar.dog>
2021-05-11 19:45:13 -0400

    Write half a delete impl

--- src/recursive.rs
+++ src/recursive.rs
@@ -63,6 +63,44 @@ impl<K, V> Tree<K, V> {
             },
         }
     }
+
+    pub fn delete(self, k: K) -> Self
+    where
+        K: cmp::Ord,
+    {
+        match self {
+            Tree::Leaf => self,
+            Tree::Node(n) => match k.cmp(&n.key) {
+                cmp::Ordering::Less => Tree::Node(Node {
+                    left: Box::new(n.left.delete(k)),
+                    ..n
+                }),
+                cmp::Ordering::Equal => match (n.left.as_ref(), n.right.as_ref()) {
+                    (Tree::Leaf, _) => *n.right,
+                    (_, Tree::Leaf) => *n.left,
+                    _ => unimplemented!(),
+                },
+                cmp::Ordering::Greater => Tree::Node(Node {
+                    right: Box::new(n.right.delete(k)),
+                    ..n
+                }),
+            },
+        }
+    }
+
+    #[allow(dead_code)]
+    fn greatest_in(self) -> Self
+    where
+        K: cmp::Ord,
+    {
+        match self {
+            Tree::Leaf => self,
+            Tree::Node(n) => match *n.right {
+                Tree::Leaf => *n.right,
+                Tree::Node(r) => r.right.greatest_in(),
+            },
+        }
+    }
 }
 
 #[cfg(test)]
@@ -71,10 +109,56 @@ mod tests {
 
     #[test]
     fn test_insert() {
-        let tree = Tree::new();
-        let tree = tree.insert(1, 2);
+        let mut tree = Tree::new();
+        tree = tree.insert(1, 2);
 
         assert_eq!(tree.find(1), Some(&2));
+    }
+
+    #[test]
+    fn test_delete_no_children() {
+        let mut tree = Tree::new();
+        tree = tree.insert(1, 2);
+        tree = tree.insert(2, 3);
+        tree = tree.delete(2);
+
+        assert_eq!(tree.find(1), Some(&2));
+        assert_eq!(tree.find(2), None);
+    }
+
+    #[test]
+    fn test_delete_no_left_child() {
+        let mut tree = Tree::new();
+        tree = tree.insert(1, 2);
+        tree = tree.insert(2, 3);
+        tree = tree.delete(1);
+
+        assert_eq!(tree.find(1), None);
+        assert_eq!(tree.find(2), Some(&3));
+    }
+
+    #[test]
+    fn test_delete_no_right_child() {
+        let mut tree = Tree::new();
+        tree = tree.insert(2, 3);
+        tree = tree.insert(1, 2);
+        tree = tree.delete(2);
+
+        assert_eq!(tree.find(1), Some(&2));
+        assert_eq!(tree.find(2), None);
+    }
+
+    #[test]
+    fn test_delete_two_children() {
+        let mut tree = Tree::new();
+        tree = tree.insert(2, 3);
+        tree = tree.insert(1, 2);
+        tree = tree.insert(3, 4);
+        tree = tree.delete(2);
+
+        assert_eq!(tree.find(1), Some(&2));
+        assert_eq!(tree.find(2), None);
+        assert_eq!(tree.find(3), Some(&4));
     }
 
     // TODO Test BST invariant
