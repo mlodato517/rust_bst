d0225b2fbdc302a7451819278a32011bf6eb78a0
Mark Lodato <mlodato517@gmail.com>
2023-03-03 14:47:05 -0500

    Add unsafe AVL Tree
    ## This Commit
    
    Adds an implementation of an AVL tree using `unsafe` and raw pointers.
    
    ## Why?
    
    To compare performance against the immutable tree and learn how to
    implement data structures requiring `unsafe` code.
    
    ## Benchmarks
    
    See the full benchmarks below the fold but the high level review is:
    
    1. finding is basically equivalent between the trees (this is expected)
    2. insert/delete is faster for the unsafe tree in general
        - How much faster? Great question and I don't _really_ know. Maybe
          ~4x but I think I should make the benchmarks more targeted for
          various kinds of rotations and things.
    
    <details>
    <summary>Results</summary>
    
    ```
    find/immutable/6            time:   [18.312 ns 18.530 ns 19.003 ns]
    find/bangsafe/6             time:   [17.497 ns 17.624 ns 17.764 ns]
    find/immutable/126          time:   [23.565 ns 23.605 ns 23.654 ns]
    find/bangsafe/126           time:   [22.644 ns 22.731 ns 22.839 ns]
    find/immutable/2046         time:   [30.506 ns 30.562 ns 30.621 ns]
    find/bangsafe/2046          time:   [29.023 ns 29.055 ns 29.095 ns]
    find/immutable/32766        time:   [40.783 ns 41.067 ns 41.370 ns]
    find/bangsafe/32766         time:   [44.619 ns 44.954 ns 45.305 ns]
    
    delete/immutable/6          time:   [53.678 ns 53.724 ns 53.775 ns]
    delete/bangsafe/6           time:   [34.415 ns 34.448 ns 34.487 ns]
    delete/immutable/126        time:   [146.14 ns 146.69 ns 147.40 ns]
    delete/bangsafe/126         time:   [52.139 ns 52.220 ns 52.306 ns]
    delete/immutable/2046       time:   [310.10 ns 310.93 ns 311.89 ns]
    delete/bangsafe/2046        time:   [73.214 ns 73.388 ns 73.596 ns]
    delete/immutable/32766      time:   [490.92 ns 498.97 ns 510.66 ns]
    delete/bangsafe/32766       time:   [130.12 ns 131.33 ns 132.58 ns]
    
    insert/immutable/6          time:   [228.69 ns 229.27 ns 230.35 ns]
    insert/bangsafe/6           time:   [122.80 ns 122.85 ns 122.92 ns]
    insert/immutable/126        time:   [250.56 ns 250.77 ns 251.01 ns]
    insert/bangsafe/126         time:   [56.900 ns 56.961 ns 57.032 ns]
    insert/immutable/2046       time:   [430.40 ns 431.41 ns 432.59 ns]
    insert/bangsafe/2046        time:   [70.494 ns 70.745 ns 71.161 ns]
    insert/immutable/32766      time:   [652.87 ns 655.95 ns 659.06 ns]
    insert/bangsafe/32766       time:   [107.08 ns 107.72 ns 108.34 ns]
    
    find-miss/immutable/6       time:   [17.797 ns 17.826 ns 17.868 ns]
    find-miss/bangsafe/6        time:   [17.745 ns 17.768 ns 17.793 ns]
    find-miss/immutable/126     time:   [23.399 ns 23.443 ns 23.495 ns]
    find-miss/bangsafe/126      time:   [24.865 ns 25.247 ns 26.027 ns]
    find-miss/immutable/2046    time:   [30.361 ns 30.429 ns 30.504 ns]
    find-miss/bangsafe/2046     time:   [29.651 ns 29.713 ns 29.786 ns]
    find-miss/immutable/32766   time:   [40.383 ns 41.047 ns 41.927 ns]
    find-miss/bangsafe/32766    time:   [41.519 ns 41.622 ns 41.732 ns]
    
    delete-miss/immutable/6     time:   [73.729 ns 73.890 ns 74.131 ns]
    delete-miss/bangsafe/6      time:   [22.151 ns 22.183 ns 22.217 ns]
    delete-miss/immutable/126   time:   [168.89 ns 169.11 ns 169.35 ns]
    delete-miss/bangsafe/126    time:   [28.771 ns 28.807 ns 28.848 ns]
    delete-miss/immutable/2046  time:   [337.73 ns 338.81 ns 340.09 ns]
    delete-miss/bangsafe/2046   time:   [42.772 ns 42.895 ns 43.041 ns]
    delete-miss/immutable/32766 time:   [514.39 ns 517.86 ns 521.50 ns]
    delete-miss/bangsafe/32766  time:   [68.273 ns 68.716 ns 69.236 ns]
    ```
    
    </details>

---
+++ benches/bench.rs
@@ -0,0 +1,124 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use bst::{bangsafe, immutable};
+
+enum TreeEnum<K, V> {
+    Bangsafe(bangsafe::Tree<K, V>),
+    Immutable(immutable::Tree<K, V>),
+}
+
+impl<K, V> TreeEnum<K, V> {
+    fn find(&self, k: &K) -> Option<&V>
+    where
+        K: Ord,
+    {
+        match self {
+            Self::Bangsafe(t) => t.find(k),
+            Self::Immutable(t) => t.find(k),
+        }
+    }
+
+    fn insert(&mut self, k: K, v: V)
+    where
+        K: Ord,
+    {
+        match self {
+            Self::Bangsafe(t) => t.insert(k, v),
+            Self::Immutable(t) => {
+                t.insert(k, v);
+            }
+        }
+    }
+
+    fn delete(&mut self, k: &K)
+    where
+        K: Ord,
+    {
+        match self {
+            Self::Bangsafe(t) => {
+                t.delete(k);
+            }
+            Self::Immutable(t) => {
+                t.delete(k);
+            }
+        }
+    }
+}
+
+/// Helper to bench a function on a BST.
+/// It creates a group for the given name and closure and runs tests for various sizes and
+/// implementations of BSTs before finishing the group.
+fn bench_helper(c: &mut Criterion, name: &str, f: impl Fn(&mut TreeEnum<i32, i32>, i32)) {
+    let mut group = c.benchmark_group(name);
+
+    for num_levels in [3, 7, 11, 15] {
+        let num_nodes = 2usize.pow(num_levels as u32) - 1;
+        // TODO consider `max` method on BST.
+        let largest_element_in_tree = 2usize.pow(num_levels as u32) - 2;
+
+        let tree_tests = ["immutable", "bangsafe"];
+        for name in tree_tests {
+            let id = BenchmarkId::new(name, largest_element_in_tree);
+
+            group.bench_function(id, |b| {
+                b.iter_custom(|iters| {
+                    let mut time = std::time::Duration::ZERO;
+                    for _ in 0..iters {
+                        let tree = match name {
+                            "immutable" => {
+                                let immutable_tree = (0..num_nodes)
+                                    .fold(immutable::Tree::new(), |tree, x| {
+                                        tree.insert(x as i32, x as i32)
+                                    });
+                                TreeEnum::Immutable(immutable_tree)
+                            }
+                            "bangsafe" => {
+                                let bangsafe_tree = {
+                                    let mut tree = bangsafe::Tree::new();
+                                    for x in 0..num_nodes {
+                                        tree.insert(x as i32, x as i32);
+                                    }
+
+                                    tree
+                                };
+                                TreeEnum::Bangsafe(bangsafe_tree)
+                            }
+                            _ => unimplemented!(),
+                        };
+                        let mut tree = black_box(tree);
+                        let instant = std::time::Instant::now();
+                        f(&mut tree, black_box(largest_element_in_tree as i32));
+                        let elapsed = instant.elapsed();
+                        time += elapsed;
+                    }
+                    time
+                })
+            });
+        }
+    }
+
+    group.finish();
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    bench_helper(c, "find", |tree, i| {
+        let _value = black_box(tree.find(&i));
+    });
+    bench_helper(c, "delete", |tree, i| {
+        tree.delete(&i);
+    });
+
+    bench_helper(c, "insert", |tree, i| {
+        tree.insert(i + 1, i + 1);
+    });
+
+    bench_helper(c, "find-miss", |tree, i| {
+        let _value = black_box(tree.find(&(i + 1)));
+    });
+    bench_helper(c, "delete-miss", |tree, i| {
+        tree.delete(&(i + 1));
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
